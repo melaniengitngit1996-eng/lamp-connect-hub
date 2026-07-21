e4e809bfcc0caff660c5f78ddfe01340dd5f9ccc
Melanie Ngitngit <melaniengitngit1996@gmail.com>
2026-07-21 09:40:16 +0800

    password preview

--- resources/js/pages/auth/LoginPage.vue
+++ resources/js/pages/auth/LoginPage.vue
@@ -119,7 +119,7 @@ input::-ms-clear {
                      Password
                   </label>
                   <div class="relative">
-                     <input class="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm" required="" minlength="6" :type="showPassword ? 'text' : 'password'">
+                     <input class="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm" id="password" required="" v-model="password" minlength="6" :type="showPassword ? 'text' : 'password'">
                      <button
                         type="button"
                         class="absolute inset-y-0 right-0 flex items-center px-3 text-muted-foreground hover:text-foreground"
