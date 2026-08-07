4bd9794d8d03151e9f0cf8e355d9267dc3cc19de
Melanie Ngitngit <melaniengitngit1996@gmail.com>
2026-08-07 17:47:41 +0800

    username fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'status'
+        'status',
+        'username'
     ];
 
     protected $appends = [
